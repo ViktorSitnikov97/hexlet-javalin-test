2bf06935d5712c0c933aee2e4cfcc1cfeff99ddb
Viktor Sitnikov <sitnikoff_victor_97@mail.ru>
2024-08-19 19:21:30 +0300

    update HelloWorld.java

--- HexletJavalin/src/main/java/org/example/hexlet/HelloWorld.java
+++ HexletJavalin/src/main/java/org/example/hexlet/HelloWorld.java
@@ -2,13 +2,22 @@ package org.example.hexlet;
 
 import io.javalin.Javalin;
 
+
 public class HelloWorld {
     public static void main(String[] args) {
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
         });
 
-        app.get("/", ctx -> ctx.result("Hello, World!"));
+        app.get("/hello", ctx -> {
+            var name = ctx.queryParam("name");
+            if (name != null) {
+                ctx.result(String.format("Hello, %s!", name));
+            } else {
+                ctx.result("Hello, World!");
+            }
+
+        });
         app.start(7070);
     }
 }
