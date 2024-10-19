f053bc27f7412ee9bf7594c63c47d9d01fd93864
Viktor Sitnikov <sitnikoff_victor_97@mail.ru>
2024-10-19 23:31:09 +0300

    update CourseRepository.java and UserRepository.java

--- HexletJavalin/src/main/java/org/example/hexlet/repository/CourseRepository.java
+++ HexletJavalin/src/main/java/org/example/hexlet/repository/CourseRepository.java
@@ -1,48 +1,140 @@
 package org.example.hexlet.repository;
 
 import org.example.hexlet.model.Course;
-import org.example.hexlet.model.User;
-
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 
-public class CourseRepository {
-    private static final List<Course> entities = new ArrayList<>();
+public class CourseRepository extends BaseRepository {
+    public static void save(Course course) throws SQLException {
+        String sql = "INSERT INTO courses (title, description) VALUES (?, ?);";
 
-    public static void save(Course course) {
-        course.setId((long) entities.size() + 1);
-        entities.add(course);
-    }
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement preparedStatement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            var title = course.getTitle();
+            var description = course.getDescription();
 
-    public static void delete(Long id) {
-        List<Course> entitiesInterim = new ArrayList<>();
-        entities.forEach(course -> {
-            if(!course.getId().equals(id)) {
-                course.setId((long) entitiesInterim.size() + 1);
-                entitiesInterim.add(course);
+            preparedStatement.setString(1, title);
+            preparedStatement.setString(2, description);
+            preparedStatement.executeUpdate();
+
+            ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
+
+            if (generatedKeys.next()) {
+                course.setId(generatedKeys.getLong("id"));
+            } else {
+                throw new SQLException("DB have not returned an id after saving an entity");
             }
-        });
-        entities.clear();
-        entities.addAll(entitiesInterim);
+        }
     }
 
-    public static List<Course> search(String term) {
-        return entities.stream()
-                .filter(entity -> entity.getName().toLowerCase()
-                        .contains(term.trim().toLowerCase()))
-                .toList();
+    public static Optional<Course> findById(Long id) throws SQLException {
+        String sql = "SELECT * FROM courses WHERE id = ?;";
+
+        try (var conn = dataSource.getConnection();
+             var preparedStatement = conn.prepareStatement(sql)) {
+            preparedStatement.setLong(1, id);
+            var resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                var title = resultSet.getString("title");
+                var description = resultSet.getString("description");
+                var course = new Course(title, description);
+                course.setId(id);
+                return Optional.of(course);
+            }
+            return Optional.empty();
+        }
     }
 
-    public static Optional<Course> find(Long id) throws Exception {
-        return Optional.ofNullable(entities.stream()
-                .filter(entity -> Objects.equals(entity.getId(), id))
-                .findAny()
-                .orElseThrow(() -> new Exception("Course not found")));
+    public static Boolean existsByTitle(String title, Long id) {
+        String sql = "SELECT * FROM courses WHERE title = ? AND id != ?;";
+
+        try (var conn = dataSource.getConnection();
+             var preparedStatement = conn.prepareStatement(sql)) {
+            preparedStatement.setString(1, title);
+            preparedStatement.setLong(2, id);
+            var resultSet = preparedStatement.executeQuery();
+            return resultSet.next();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public static List<Course> getEntities() {
-        return entities;
+    public static List<Course> getEntities() throws SQLException {
+
+        String sql = "SELECT * FROM courses;";
+        List<Course> courses = new ArrayList<>();
+
+        try (var conn = dataSource.getConnection();
+             var statement = conn.createStatement()) {
+            var resultSet = statement.executeQuery(sql);
+
+            while (resultSet.next()) {
+                var title = resultSet.getString("title");
+                var description = resultSet.getString("description");
+                var id = resultSet.getLong("id");
+
+                var course = new Course(title, description);
+                course.setId(id);
+                courses.add(course);
+            }
+            return courses;
+        }
+    }
+
+    public static List<Course> search(String term) throws SQLException {
+        String sql = "SELECT * FROM courses WHERE title ILIKE ?;";
+        List<Course> courses = new ArrayList<>();
+        try (var conn = dataSource.getConnection();
+             var preparedStatement = conn.prepareStatement(sql)) {
+            String searchPattern = term + "%";
+            preparedStatement.setString(1, searchPattern);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                var title = resultSet.getString("title");
+                var id = resultSet.getLong("id");
+
+                var course = new Course(title, null);
+                course.setId(id);
+                courses.add(course);
+            }
+            return courses;
+        }
+    }
+
+    public static void delete(Long id) throws SQLException {
+        String sql = "DELETE FROM courses WHERE id = ?;";
+        try (var conn = dataSource.getConnection();
+             var preparedStatement = conn.prepareStatement(sql)) {
+            preparedStatement.setLong(1, id);
+            var count = preparedStatement.executeUpdate();
+            if (count != 1) {
+                throw new SQLException("Course has not been deleted");
+            }
+        }
+    }
+
+    public static void update(Course course) throws SQLException {
+        String sql = "UPDATE courses SET title = ?, description = ? WHERE id = ?;";
+
+        try (var conn = dataSource.getConnection();
+             var preparedStatement = conn.prepareStatement(sql)) {
+
+            var title = course.getTitle();
+            var description = course.getDescription();
+            var id = course.getId();
+
+            preparedStatement.setString(1, title);
+            preparedStatement.setString(2, description);
+            preparedStatement.setLong(3, id);
+            preparedStatement.executeUpdate();
+        }
     }
 }
--- HexletJavalin/src/main/java/org/example/hexlet/repository/UserRepository.java
+++ HexletJavalin/src/main/java/org/example/hexlet/repository/UserRepository.java
@@ -1,50 +1,180 @@
 package org.example.hexlet.repository;
 
-import io.javalin.http.NotFoundResponse;
-import org.example.hexlet.model.User;
 
+import org.example.hexlet.model.User;
+import org.example.hexlet.util.Data;
+import org.example.hexlet.util.Security;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
-public final class UserRepository {
-    private static final List<User> entities = new ArrayList<>();
 
-    public static void save(User user) {
-        user.setId((long) entities.size() + 1);
-        entities.add(user);
-    }
+public final class UserRepository extends BaseRepository {
 
-    public static void delete(Long id) {
-        List<User> entitiesInterim = new ArrayList<>();
-        entities.forEach(user -> {
-            if(!user.getId().equals(id)) {
-                user.setId((long) entitiesInterim.size() + 1);
-                entitiesInterim.add(user);
+    public static void save(User user) throws SQLException {
+        String sql = "INSERT INTO users (name, email, password) VALUES (?, ?, ?);";
+
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement preparedStatement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            var name = user.getName();
+            var email = user.getEmail();
+            var password = Security.encrypt(user.getPassword());
+
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, email);
+            preparedStatement.setString(3, password);
+            preparedStatement.executeUpdate();
+
+            ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
+
+            if (generatedKeys.next()) {
+                user.setId(generatedKeys.getLong("id"));
+            } else {
+                throw new SQLException("DB have not returned an id after saving an entity");
             }
-        });
-        entities.clear();
-        entities.addAll(entitiesInterim);
+        }
     }
 
-    public static List<User> search(String term) {
-        return entities.stream()
-                .filter(u -> u.getName().toLowerCase()
-                        .contains(term.trim().toLowerCase()))
-                .collect(Collectors.toCollection(ArrayList::new));
+    public static Optional<User> findById(Long id) throws SQLException {
+        String sql = "SELECT * FROM users WHERE id = ?;";
+
+        try (var conn = dataSource.getConnection();
+                var preparedStatement = conn.prepareStatement(sql)) {
+            preparedStatement.setLong(1, id);
+            var resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                var name = resultSet.getString("name");
+                var email = resultSet.getString("email");
+                var password = resultSet.getString("password");
+                var user = new User(name, email, password);
+                user.setId(id);
+                return Optional.of(user);
+            }
+            return Optional.empty();
+        }
     }
 
-    public static Optional<User> find(Long id) throws Exception {
-        return Optional.ofNullable(entities.stream()
-                .filter(u -> Objects.equals(u.getId(), id))
-                .findFirst()
-                .orElseThrow(() -> new Exception("User not found")));
+    public static Boolean existsByEmail(String email, Long id) {
+        String sql = "SELECT * FROM users WHERE email = ? AND id != ?;";
+
+        try (var conn = dataSource.getConnection();
+             var preparedStatement = conn.prepareStatement(sql)) {
+            preparedStatement.setString(1, email);
+            preparedStatement.setLong(2, id);
+            var resultSet = preparedStatement.executeQuery();
+            return resultSet.next();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public static List<User> getEntities() {
-        return entities;
+    public static List<User> getEntities() throws SQLException {
+
+        String sql = "SELECT * FROM users;";
+        List<User> users = new ArrayList<>();
+
+        try (var conn = dataSource.getConnection();
+                var statement = conn.createStatement()) {
+            var resultSet = statement.executeQuery(sql);
+
+            while (resultSet.next()) {
+                var name = resultSet.getString("name");
+                var email = resultSet.getString("email");
+                var id = resultSet.getLong("id");
+                var password = resultSet.getString("password");
+
+                var user = new User(name, email, password);
+                user.setId(id);
+                users.add(user);
+            }
+            return users;
+        }
     }
 
+    public static List<User> search(String term) throws SQLException {
+        String sql = "SELECT * FROM users WHERE name ILIKE ?;";
+        List<User> users = new ArrayList<>();
+        try (var conn = dataSource.getConnection();
+                var preparedStatement = conn.prepareStatement(sql)) {
+            String searchPattern = term + "%";
+            preparedStatement.setString(1, searchPattern);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                var name = resultSet.getString("name");
+                var id = resultSet.getLong("id");
+
+                var user = new User(name, null, null);
+                user.setId(id);
+                users.add(user);
+            }
+            return users;
+        }
+    }
+
+    public static void delete(Long id) throws SQLException {
+        String sql = "DELETE FROM users WHERE id = ?;";
+        try (var conn = dataSource.getConnection();
+                var preparedStatement = conn.prepareStatement(sql)) {
+            preparedStatement.setLong(1, id);
+            var count = preparedStatement.executeUpdate();
+            if (count != 1) {
+                throw new SQLException("User has not been deleted");
+            }
+        }
+    }
+
+    public static void update(User user) throws SQLException {
+        String sql = "UPDATE users SET name = ?, email = ?, password = ? WHERE id = ?;";
+
+        try (var conn = dataSource.getConnection();
+                var preparedStatement = conn.prepareStatement(sql)) {
+
+            var name = user.getName();
+            var email = user.getEmail();
+            var password = user.getPassword();
+            var id = user.getId();
+
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, email);
+            preparedStatement.setString(3, password);
+            preparedStatement.setLong(4, id);
+            preparedStatement.executeUpdate();
+        }
+    }
+
+    public static void addFakeUsers() throws SQLException {
+        String sql = "INSERT INTO users (name, email, password) VALUES (?, ?, ?);";
+
+        try (var conn = dataSource.getConnection();
+                var preparedStatement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            List<List<String>> fakeUsers = Data.getUsers();
+
+            for (var user : fakeUsers) {
+                var name = user.get(0);
+                var email = user.get(1);
+                var password = Security.encrypt(user.get(2));
+
+                preparedStatement.setString(1, name);
+                preparedStatement.setString(2, email);
+                preparedStatement.setString(3, password);
+                preparedStatement.executeUpdate();
+                var generatedKeys = preparedStatement.getGeneratedKeys();
+
+                var currentUser = new User(name, email, password);
+                if (generatedKeys.next()) {
+                    currentUser.setId(generatedKeys.getLong("id"));
+                } else {
+                    throw new SQLException("DB have not returned an id after saving an entity");
+                }
+            }
+        }
+    }
 }
