af3c90c2c4e8f2e3e5c4f4e13bcd3685f359b8b5
Viktor Sitnikov <sitnikoff_victor_97@mail.ru>
2024-10-19 23:29:48 +0300

    update UsersController.java and CoursesController.java

--- HexletJavalin/src/main/java/org/example/hexlet/controllers/CoursesController.java
+++ HexletJavalin/src/main/java/org/example/hexlet/controllers/CoursesController.java
@@ -2,26 +2,21 @@ package org.example.hexlet.controllers;
 
 import io.javalin.http.Context;
 import io.javalin.validation.ValidationException;
-import org.apache.commons.lang3.StringUtils;
 import org.example.hexlet.dto.courses.BuildCoursePage;
 import org.example.hexlet.dto.courses.CoursePage;
 import org.example.hexlet.dto.courses.CoursesPage;
 import org.example.hexlet.dto.courses.EditCoursePage;
-import org.example.hexlet.dto.users.BuildUserPage;
-import org.example.hexlet.dto.users.UserPage;
-import org.example.hexlet.dto.users.UsersPage;
 import org.example.hexlet.model.Course;
-import org.example.hexlet.model.User;
 import org.example.hexlet.repository.CourseRepository;
-import org.example.hexlet.repository.UserRepository;
 import org.example.hexlet.util.NamedRoutes;
 
+import java.sql.SQLException;
 import java.util.List;
 
 import static io.javalin.rendering.template.TemplateUtil.model;
 
 public class CoursesController {
-    public static void index(Context ctx) {
+    public static void index(Context ctx) throws SQLException {
         var term = ctx.queryParam("term");
         List<Course> courses;
         if (term != null) {
@@ -31,12 +26,14 @@ public class CoursesController {
         }
 
         var page = new CoursesPage(courses, term);
+        page.setFlash(ctx.consumeSessionAttribute("flash"));
+        page.setFlag(true);
         ctx.render("courses/index.jte", model("page", page));
     }
 
     public static void show(Context ctx) throws Exception {
         var id = ctx.pathParamAsClass("id", Long.class).get();
-        var course = CourseRepository.find(id).get();
+        var course = CourseRepository.findById(id).get();
         var page = new CoursePage(course);
         ctx.render("courses/show.jte", model("page", page));
     }
@@ -46,63 +43,77 @@ public class CoursesController {
         ctx.render("courses/build.jte", model("page", page));
     }
 
-    public static void create(Context ctx) {
+    public static void create(Context ctx) throws SQLException {
+
         try {
-            var name = ctx.formParamAsClass("name", String.class)
+            var title = ctx.formParamAsClass("title", String.class)
                     .check(value -> value.length() > 2, "Название курса менее 3 символов")
+                    .check(value -> !CourseRepository.existsByTitle(value, 0L), "Курс с таким названием уже существует")
                     .get();
             var description = ctx.formParamAsClass("description", String.class)
                     .check(value -> value.length() > 10, "Описание курса менее 10 символов")
                     .get();
-            var course = new Course(name, description);
+            var course = new Course(title, description);
             CourseRepository.save(course);
+            ctx.sessionAttribute("flash", "Course has been created!");
+
             ctx.redirect("/courses");
         } catch (ValidationException e) {
-            var name = ctx.formParam("name");
+            var title = ctx.formParam("course");
             var description = ctx.formParam("description");
-            var page = new BuildCoursePage(name, description, e.getErrors());
+            var page = new BuildCoursePage(title, description, e.getErrors());
+            ctx.sessionAttribute("flash", "Course has not been created!");
+            page.setFlash(ctx.consumeSessionAttribute("flash"));
+            page.setFlag(false);
             ctx.render("courses/build.jte", model("page", page));
         }
     }
 
     public static void edit(Context ctx) throws Exception {
         var id = ctx.pathParamAsClass("id", Long.class).get();
-        var course = CourseRepository.find(id).get();
-        var name = course.getName();
+        var course = CourseRepository.findById(id).get();
+        var title = course.getTitle();
         var description = course.getDescription();
-        var page = new EditCoursePage(id, name, description, null);
+        var page = new EditCoursePage(id, title, description, null);
         ctx.render("courses/edit.jte", model("page", page));
     }
 
     public static void update(Context ctx) throws Exception {
-        var id = ctx.pathParamAsClass("id", Long.class).get();
 
-        var name = ctx.formParam("name");
+        var id = ctx.pathParamAsClass("id", Long.class).get();
+        var title = ctx.formParam("title");
         var description = ctx.formParam("description");
+        var courseCurrent = CourseRepository.findById(id).get();
 
         try{
-            var nameNew = ctx.formParamAsClass("name", String.class)
+            var titleNew = ctx.formParamAsClass("title", String.class)
                     .check(value -> value.length() > 2, "Название курса менее 3 символов")
+                    .check(value -> !CourseRepository.existsByTitle(value, courseCurrent.getId()), "Курс с таким названием уже существует")
                     .get();
             var descriptionNew = ctx.formParamAsClass("description", String.class)
                     .check(value -> value.length() > 10, "Описание курса менее 10 символов")
                     .get();
 
-            var course = CourseRepository.find(id).get();
-            course.setName(nameNew);
+            var course = CourseRepository.findById(id).get();
+            course.setTitle(titleNew);
             course.setDescription(descriptionNew);
-
+            CourseRepository.update(course);
+            ctx.sessionAttribute("flash", "Course has been updated!");
             ctx.redirect(NamedRoutes.coursesPath());
 
         } catch (ValidationException e) {
-            var page = new EditCoursePage(id, name, description, e.getErrors());
+            var page = new EditCoursePage(id, title, description, e.getErrors());
+            ctx.sessionAttribute("flash", "Course has not been updated!");
+            page.setFlash(ctx.consumeSessionAttribute("flash"));
+            page.setFlag(false);
             ctx.render("courses/edit.jte", model("page", page));
         }
     }
 
-    public static void destroy(Context ctx) {
+    public static void destroy(Context ctx) throws SQLException {
         var id = ctx.pathParamAsClass("id", Long.class).get();
         CourseRepository.delete(id);
+        ctx.sessionAttribute("flash", "Course has been deleted!");
         ctx.redirect(NamedRoutes.coursesPath());
     }
 }
--- HexletJavalin/src/main/java/org/example/hexlet/controllers/UsersController.java
+++ HexletJavalin/src/main/java/org/example/hexlet/controllers/UsersController.java
@@ -12,13 +12,14 @@ import org.example.hexlet.repository.UserRepository;
 import io.javalin.http.Context;
 import org.example.hexlet.util.NamedRoutes;
 
+import java.sql.SQLException;
 import java.util.List;
 
 import static io.javalin.rendering.template.TemplateUtil.model;
 
 public class UsersController {
 
-    public static void index(Context ctx) {
+    public static void index(Context ctx) throws SQLException {
         var term = ctx.queryParam("term");
         List<User> users;
         if (term != null) {
@@ -35,7 +36,7 @@ public class UsersController {
 
     public static void show(Context ctx) throws Exception {
         var id = ctx.pathParamAsClass("id", Long.class).get();
-        var user = UserRepository.find(id).get();
+        var user = UserRepository.findById(id).get();
         var page = new UserPage(user);
         ctx.render("users/show.jte", model("page", page));
     }
@@ -45,22 +46,25 @@ public class UsersController {
         ctx.render("users/build.jte", model("page",page));
     }
 
-    public static void create(Context ctx) {
+    public static void create(Context ctx) throws SQLException {
         var name = StringUtils.capitalize(ctx.formParam("name").trim());
-        var email = ctx.formParam("email").trim().toLowerCase();
 
-        try{
+        try {
             var passwordConfirmation = ctx.formParam("passwordConfirmation");
             var password = ctx.formParamAsClass("password", String.class)
                     .check(value -> value.equals(passwordConfirmation), "Пароли не совпадают")
                     .check(value -> value.length() >= 4, "Пароль менее 4 символов")
                     .get();
+            var emailCurrent = ctx.formParamAsClass("email", String.class)
+                    .check(value -> !UserRepository.existsByEmail(normalizeEmail(value), 0L), "Пользователь с такой почтой уже существует")
+                    .get();
+            var email = normalizeEmail(emailCurrent);
             var user = new User(name, email, password);
             UserRepository.save(user);
             ctx.sessionAttribute("flash", "User has been added!");
             ctx.redirect(NamedRoutes.usersPath());
         } catch (ValidationException e) {
-            var page = new BuildUserPage(name, email, e.getErrors());
+            var page = new BuildUserPage(name, e.getErrors());
             ctx.sessionAttribute("flash", "User has not been added!");
             page.setFlash(ctx.consumeSessionAttribute("flash"));
             page.setFlag(false);
@@ -68,13 +72,16 @@ public class UsersController {
         }
     }
 
+    private static String normalizeEmail(String email) {
+        return email.trim().toLowerCase();
+    }
+
     public static void edit(Context ctx) throws Exception {
         var id = ctx.pathParamAsClass("id", Long.class).get();
-        var user = UserRepository.find(id).get();
+        var user = UserRepository.findById(id).get();
         var name = user.getName();
         var email = user.getEmail();
-        var password = user.getPassword();
-        var page = new EditUserPage(id, name, email, password, null);
+        var page = new EditUserPage(id, name, email, null, null);
         ctx.render("users/edit.jte", model("page", page));
     }
 
@@ -82,25 +89,29 @@ public class UsersController {
 
         var id = ctx.pathParamAsClass("id", Long.class).get();
         var name = StringUtils.capitalize(ctx.formParam("name").trim());
-        var email = ctx.formParam("email").trim().toLowerCase();
+        var emailCurrent = ctx.formParam("email").trim().toLowerCase();
 
-        try{
+        try {
             var passwordConfirmation = ctx.formParam("passwordConfirmation");
             var password = ctx.formParamAsClass("password", String.class)
                     .check(value -> value.equals(passwordConfirmation), "Пароли не совпадают")
                     .check(value -> value.length() >= 4, "Пароль менее 4 символов")
                     .get();
+            var email = normalizeEmail(ctx.formParamAsClass("email", String.class)
+                    .check(value -> !UserRepository.existsByEmail(emailCurrent, id), "Такая почта уже используется")
+                    .get());
 
-            var user = UserRepository.find(id).get();
+            var user = UserRepository.findById(id).get();
             user.setName(name);
             user.setEmail(email);
             user.setPassword(password);
+            UserRepository.update(user);
             ctx.sessionAttribute("flash", "User has been updated!");
             ctx.redirect(NamedRoutes.usersPath());
 
         } catch (ValidationException e) {
 //            var password = ctx.formParam("password");
-            var page = new EditUserPage(id, name, email, null, e.getErrors());
+            var page = new EditUserPage(id, name, emailCurrent, null, e.getErrors());
             ctx.sessionAttribute("flash","User has not been updated!");
             page.setFlash(ctx.consumeSessionAttribute("flash"));
             page.setFlag(false);
@@ -108,7 +119,7 @@ public class UsersController {
         }
     }
 
-    public static void destroy(Context ctx) {
+    public static void destroy(Context ctx) throws SQLException {
         var id = ctx.pathParamAsClass("id", Long.class).get();
         UserRepository.delete(id);
         ctx.sessionAttribute("flash", "User has been deleted!");
